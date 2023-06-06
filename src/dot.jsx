e065edbdba64ae0f17def4ba5204042cc3d710cc
Aviral Dhingra <aviral.dhingra.2008@gmail.com>
2023-06-06 10:16:34 +0530

    Complete structure + some animations

---
+++ src/dot.jsx
@@ -0,0 +1,21 @@
+import { React } from "react";
+
+function Dot() {
+  return (
+    <svg
+      width="22"
+      height="22"
+      viewBox="0 0 22 22"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        opacity="0.4"
+        d="M10.9509 21.8744C16.9989 21.8744 21.9017 16.9777 21.9017 10.9372C21.9017 4.89676 16.9989 0 10.9509 0C4.90287 0 0 4.89676 0 10.9372C0 16.9777 4.90287 21.8744 10.9509 21.8744Z"
+        fill="#F3F4F6"
+      ></path>
+    </svg>
+  );
+}
+
+export default Dot;
